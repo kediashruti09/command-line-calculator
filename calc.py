aadc6182d6e5263aea1ee83a6822de75cf380c9c
kediashruti09 <123299225+kediashruti09@users.noreply.github.com>
2023-07-15 14:28:01 +0530

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,11 +1,18 @@
-#code for cli calculator
+#code for CLI calculator
+import argparse
 import math
-num1=float(input("Enter the first number:"))
-num2 =float(input("Enter the second number:"))
-operation =input("choose the operation(+,-,*,/,max,min,log,power,square root,gcd):")
 
-        #perform calculation based on user input
 
+        #perform a calculation based on user input
+
+parser = argparse.ArgumentParser(description ='Take two integer or float value-')
+parser.add_argument("num1",type=float,help='give a positive value for first variable')
+parser.add_argument("num2",type=float,help='give a positive value for second variable')
+parser.add_argument("operation",choices=['+','-','*','/','max','min','min','log','power','square root','gcd'],help="the operation must be +,-,*,/,log,max,min,square root,gcd")
+args = parser.parse_args()
+num1= args.num1
+num2=args.num2
+operation=args.operation
 if operation == "+":
     result = num1+num2
     print(round(result,3))
@@ -41,6 +48,7 @@ elif operation == "log":
         print(round(result2,3))
     else:
         print("invalid number entered")
+
 elif operation == "square root":
     if num1 >=0 and num2 >=0:
         result1=math.sqrt(num1)
@@ -71,3 +79,5 @@ else:
 
 
 
+
+   
